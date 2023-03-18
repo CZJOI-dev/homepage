092c3f37dc9681f667f9fcbca307fdb579238814
lzx2209 <84135537+lzx2209@users.noreply.github.com>
2023-03-18 15:46:59 +0800

    1st

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//控制台输出
+//console.clear();
+let styleTitle1 = `
+font-size: 20px;
+font-weight: 600;
+color: rgb(244,167,89);
+`
+let styleTitle2 = `
+font-size:12px;
+color: rgb(244,167,89);
+`
+let styleContent = `
+color: rgb(30,152,255);
+`
+let title1 = 'CZJ-首页'
+let title2 = `
+Theme made by Nutssss.
+Console output made by imsyy.
+Modified by CZJ-dev.                                                                           
+`
+let content = `
+版 本 号：0.1
+更新日期：2023-3-18
+
+Github: https://github.com/CZJOI-dev/homepage
+`
+console.log(`%c${title1} %c${title2} %c${content}`, styleTitle1, styleTitle2, styleContent)
